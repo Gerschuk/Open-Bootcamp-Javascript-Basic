28a29b4cce5c8834fcae621343411fff6cf8d660
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-02-23 12:37:56 -0300

    Feature:Exercise-02 finished

---
+++ ejercicio-02/ejercicio-02.js
@@ -0,0 +1,17 @@
+const lista = [
+    "Gabriel",
+    36,
+    true,
+    new Date(1986, 5, 16).toDateString(),
+    {
+        titulo: "Algorithms 4th edition",
+        autores: [
+            "Robert Sedgewick",
+            "Kevin Wayne"
+        ],
+        fecha: new Date(2011, 3, 3).toDateString(),
+        url: "https://www.amazon.com/dp/1848000693?tag=bbookshub-20&linkCode=ogi&th=1&psc=1"
+    }
+];
+
+console.log(lista);
